14c2538f9a858119b94886034f69237ae8ccc860
Oskari Ahonen <ahonenoskari@gmail.com>
2025-01-09 18:05:48 +0200

    Add some stuff

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // @ts-check
 
-console.log('github-activity');
+
+async function main() {
+    console.log('github-activity');
+
+    getActivity('Oskahon');
+
+}
+
+async function getActivity(username) {
+    // Fetch from https://api.github.com/users/<username>/events
+    console.log(`Fetching activity data for ${username}`);
+
+    // Turn into object
+}
+
+main();
