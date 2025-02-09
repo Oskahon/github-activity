1249b0d9a92e90e66e3e237a79750d441a306516
Oskari Ahonen <ahonenoskari@gmail.com>
2025-02-09 15:15:45 +0200

    Add some layout ideas and styling

--- renderer.js
+++ renderer.js
@@ -1,12 +1,1 @@
 console.log('hello electron');
-
-let count = 0;
-
-const counter = document.getElementById('count');
-counter.innerText = count;
-
-const up = document.getElementById('countUp');
-up.addEventListener('click', () => {
-    count++;
-    counter.innerHTML = count;
-});
