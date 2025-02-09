c1cb31033d703d33b8a4eef44be07dbbfe3824b0
Oskari Ahonen <ahonenoskari@gmail.com>
2025-02-09 19:04:21 +0200

    Add hardcoded activity list and styling

--- renderer.js
+++ renderer.js
@@ -1,1 +1,28 @@
 console.log('hello electron');
+
+const tempListOfActivity = [
+    "In March:",
+    "Pushed 2 commits to Oskahon/github-activity",
+    "Pushed 3 commits to Oskahon/github-activity",
+    "Pushed 1 commit to Oskahon/github-activity",
+    "Created Oskahon/github-activity"
+];
+
+console.log(tempListOfActivity);
+
+const activity_list = document.getElementById('activity-list');
+
+const list = document.createElement('ul');
+
+const month = document.createElement('h2');
+month.innerText = tempListOfActivity[0];
+activity_list.append(month);
+
+for (let i = 1; i < tempListOfActivity.length; i++) {
+    const list_item = document.createElement('li');
+    list_item.innerText = tempListOfActivity[i];
+
+    list.append(list_item);
+}
+
+activity_list.append(list);
